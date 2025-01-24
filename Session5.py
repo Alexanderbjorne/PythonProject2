a9a3b0cd75238fdc4b0dfd9bdebefeead232cd0d
alexanderbjorne <abjrne.ieu2023@student.ie.edu>
2025-01-24 11:53:22 +0100

    Renamed object

--- Session5.py
+++ Session5.py
@@ -1,20 +1,20 @@
 name = input("What is your name? ")
-capse = name.capitalize()
-print("Hello", capse)
+capitalized_name = name.capitalize()
+print("Hello", capitalized_name)
 #
 # ### Year you were born
 # age = input("How old are you "+capse+"? ")
 # print(capse+" based on my advanced calculations, you were born in "+str(2024-int(age))+".")
 #
 ### Simpler Way
-age2 = input(f"How old are you {capse}? ")
+age2 = input(f"How old are you {capitalized_name}? ")
 try:
     age2 = int(age2) #Here there can be a problem
-    print(f"{capse} based on my advanced calculations, you were born in {2024-age2}.")
+    print(f"{capitalized_name} based on my advanced calculations, you were born in {2024 - age2}.")
 except ValueError:
     print("Please enter a valid value for age.")
-    age2 = input(f"How old are you {capse}? ")
+    age2 = input(f"How old are you {capitalized_name}? ")
     age2 = int(age2)
-    print(f"{capse} based on my advanced calculations, you were born in {2024-age2}.")
+    print(f"{capitalized_name} based on my advanced calculations, you were born in {2024 - age2}.")
 except:
     print("This is another type of error.")
