719337e03bab27b1e3bc05586a0a8437197dc01c
alexanderbjorne <abjrne.ieu2023@student.ie.edu>
2025-01-24 11:13:47 +0100

    When you Were Born

--- Session5.py
+++ Session5.py
@@ -1,3 +1,7 @@
 name = input("What is your name? ")
 capse = name.capitalize()
 print("Hello", capse)
+
+### Year you were born
+age = input("How old are you "+capse+"? ")
+print(capse+" based on my advanced calculations, you were born in "+str(2024-int(age))+".")
