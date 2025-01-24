4f447bfe9902c6a33462881258414d78e2def8c6
alexanderbjorne <abjrne.ieu2023@student.ie.edu>
2025-01-24 11:18:19 +0100

    When you Were Born (Simpler Way)

--- Session5.py
+++ Session5.py
@@ -5,3 +5,8 @@ print("Hello", capse)
 ### Year you were born
 age = input("How old are you "+capse+"? ")
 print(capse+" based on my advanced calculations, you were born in "+str(2024-int(age))+".")
+
+### Simpler Way
+age2 = input(f"How old are you {capse}? ")
+age2 = int(age2)
+print(f"{capse} based on my advanced calculations, you were born in {2024-age2}.")
